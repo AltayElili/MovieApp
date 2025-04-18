de639d4eadf5af467f814488158e3e70e96fd07b
AltayElili <altayalili28@gmail.com>
2025-04-18 02:46:10 -0700

    commit

---
+++ app/src/main/java/com/example/mova/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.mova.utils
+
+object Constants {
+    const val API_TOKEN = "ac37f4861c5c1f1755ea4bccb69d0fa7"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val BASE_IMG_URL = ""
+    const val DEFAULT_MOVIE_IMG = ""
+}
